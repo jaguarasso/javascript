93b2ce2a71f7b650422be21472f3bba4fb6f3bbc
Thiago Bassete <taraujo438.ta@gmail.com>
2024-01-09 22:27:22 -0300

    condições

---
+++ aula05/ex004.js
@@ -0,0 +1,8 @@
+var vel = 50
+var velMax = 50
+
+if(vel<=velMax){
+    console.log(`a velocidade do seu carro e ${vel}`)
+    console.log('dirija sempre usando o cinto de segurança')}
+else{
+    console.log(`voce ultrapassou a velocidade permitida, sua velocidade ${vel}, velocidade maxima permitida ${velMax}. voce foi multado`)}
