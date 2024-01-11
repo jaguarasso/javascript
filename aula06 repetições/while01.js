397df2ef716f3bd40ce44e9e52860bba7b8ed806
Thiago Bassete <taraujo438.ta@gmail.com>
2024-01-11 00:43:15 -0300

    repetições

---
+++ aula06 repetições/while01.js
@@ -0,0 +1,12 @@
+var c=1
+
+do{
+    console.log('voce vai comprar a vista aquela mercedes em 2 anos')
+    c++
+}while(c <= 6)
+
+/*var c = 1
+while(c<7){
+    console.log('voce vai ter aquela mercedes')
+    c++
+}*/
